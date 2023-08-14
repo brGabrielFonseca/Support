645913cc7f41e9445231719d35e343a4b0e028a4
brGabrielFonseca <brgabrielfonseca@gmail.com>
2023-08-14 14:02:21 -0300

    Initial Commit

---
+++ Support/SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportApp.swift
+//  Support
+//
+//  Created by Gabriel Fonseca on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
